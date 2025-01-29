2c443c9be92e0ee250c88435788c5a7572b77bcb
Products <andreydaynyak21@gmail.com>
2025-01-29 22:40:23 +0300

    Убрал метод

--- PRODUCT.py
+++ PRODUCT.py
@@ -13,23 +13,20 @@ class Product:
         self.price=price
         self.expired=False
 
-    def create_product(self):
-        product_name=input('Name of the product: ')
-        delivery_data=input('Delivery data: ')
-        shelf_life=input('Shelf life: ')
-        price=input('Price: ')
-        expired=input(bool('Expired?: '))
-        product = Product(product_name, delivery_data, shelf_life, price,expired)
-        return product
+
+
+
 
 milk=Product('milk', '20.01.25', '14 days', 90, False)
 cheese=Product('cheese', '18.01.25', '180 days', 350, False)
 beer=Product('Zhigulevskoe', '15.01.25', '180 days', 100, False)
 water=Product('Narzan', '29.01.25', '100 days', 50, False)
 
-#Added a word
+
 
 def product_info(self):
     return f"{self.product_name},{self.delivery_data},{self.shelf_life},{self.price},{self.expired}"
 
 
+
+print(product_info(Product))
